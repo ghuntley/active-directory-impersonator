232d4ead5397ddec6efd0e33152a40466bbd0ce8
Geoffrey Huntley <ghuntley@ghuntley.com>
2011-10-04 17:28:10 +1100

    initial commit

---
+++ active-directory-impersonator/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace com.ghuntley.toolbox
+{
+    internal class Config
+    {
+        internal static string adDomain = WebConfigurationManager.AppSettings["adDomain"].ToString();
+        internal static string adUsername = WebConfigurationManager.AppSettings["adUsername"].ToString();
+        internal static string adPassword = WebConfigurationManager.AppSettings["adPassword"].ToString();
+    }
+}
---
+++ active-directory-impersonator/debug.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace com.ghuntley.toolbox
+{
+    public partial class debug : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("ASP.NET running as: " + Environment.UserName + "<br/>");
+            Response.Write("Authenticated as: " + User.Identity.Name + "<br/>");
+            using (new Impersonator(Config.adUsername, Config.adDomain, Config.adPassword))
+            {
+                Response.Write("ASP.NET (after impersonation) running as: " + Environment.UserName + "<br/>");
+                Response.Write("Authenticated (after impersonation) as: " + User.Identity.Name + "<br/>");
+            }
+        }
+    }
+}
